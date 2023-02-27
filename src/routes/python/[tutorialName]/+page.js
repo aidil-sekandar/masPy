6e0a326bd3f73ef89b7873da97053427b851890e
Aidil Iskandar <aidiliskandar63@gmail.com>
2023-02-27 15:00:37 +0800

    add python content (variable)

--- src/routes/python/[tutorialName]/+page.js
+++ src/routes/python/[tutorialName]/+page.js
@@ -1,7 +1,7 @@
 /** @type {import('./$types').PageLoad} */
 export const load = async ({ params, fetch}) => {
   let title = params.tutorialName;
-  const res = await fetch(`https://raw.githubusercontent.com/aidil-sekandar/masPy/main/static/contents/python/${title}.md`);
+  const res = await fetch(`https://raw.githubusercontent.com/aidil-sekandar/masPy/main/static/tutorial/python/${title}.md`);
   const data = await res.text()
 
   return {
